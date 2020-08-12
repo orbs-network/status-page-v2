8aa2e0bbbd179c244b216c341bf9bc1bb6f0d7bd
Or Lavy <orlavyil@gmail.com>
2020-08-12 22:25:52 +0300

    Changes and improves table

--- src/status-page-client/src/components/statusTable/StatusTable.tsx
+++ src/status-page-client/src/components/statusTable/StatusTable.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import MaterialTable from "material-table";
+import React, {useMemo} from 'react';
+import MaterialTable, {Column} from "material-table";
 import {TABLE_ICONS} from "../tables/TableIcons";
+import {Paper, Table, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import { VirtualChain, Guardian,  } from '../../../../model/model';
 
-export interface StatusGist {
+export interface ValidatorStatusGist {
+   name: string;
+   ip: string;
+}
 
+export interface VcGist {
+   id: string;
+   name: string;
 }
 
 interface IProps {
-
+   vcs: VirtualChain[],
+   committeeValidators: Guardian[],
+   standByValidators: Guardian[],
 }
 
 export const StatusTable = React.memo<IProps>(props => {
-   return <MaterialTable icons={TABLE_ICONS} columns={[]} data={[]}/>
+   const { vcs, committeeValidators, standByValidators } = props;
+   console.log(vcs);
+   console.log(committeeValidators);
+
+   const vcsRow = useMemo(() => {
+      return <TableRow>
+         <TableCell ></TableCell>
+         <TableCell >Validators</TableCell>
+         <TableCell>Node services</TableCell>
+      </TableRow>
+   }, []);
+
+   // return <MaterialTable icons={TABLE_ICONS} columns={columns} data={validatorStatusGists}/>
+   return <TableContainer component={Paper}>
+      <Table>
+         <TableHead>
+            <TableRow>
+               <TableCell>Validators</TableCell>
+               <TableCell>Node services</TableCell>
+               {vcs.map(vc => <TableCell key={vc.Id}>{vc.Id} - {vc.Name}</TableCell>)}
+            </TableRow>
+            {vcsRow}
+         </TableHead>
+      </Table>
+   </TableContainer>
 });
--- src/status-page-client/src/pages/StatusPage.tsx
+++ src/status-page-client/src/pages/StatusPage.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {observer} from "mobx-react";
 import {useStatusStore} from "../store/storeHooks";
 import {toJS} from "mobx";
 import {Page} from "../components/structure/Page";
-import {StatusTable} from "../components/statusTable/StatusTable";
+import {StatusTable, ValidatorStatusGist, VcGist} from "../components/statusTable/StatusTable";
 
 interface IProps {
 
@@ -14,7 +14,29 @@ export const StatusPage = observer<React.FunctionComponent<IProps>>(props => {
 
    console.log(toJS(statusStore.statusModel));
 
+   const virtualChains = statusStore?.statusModel?.VirtualChains;
+
+
+   const committeeNodes = statusStore?.statusModel?.CommitteeNodes;
+   const standByNodes = statusStore?.statusModel?.StandByNodes;
+   const validatorStatusGists = useMemo<ValidatorStatusGist[]>(() => {
+      if (!committeeNodes || !standByNodes) {
+         return  [];
+      }
+
+      const committeeValidatorStatusGists: ValidatorStatusGist[] = Object.entries(committeeNodes).map(([guardianId, guardian]) => {
+         const validatorStatusGist: ValidatorStatusGist = {
+            ip: guardian.Ip,
+            name: guardian.Name
+         }
+
+         return validatorStatusGist;
+      });
+
+      return committeeValidatorStatusGists;
+   }, [committeeNodes, standByNodes]);
+
    return <Page>
-      <StatusTable/>
+      <StatusTable vcs={virtualChains || []} committeeValidators={committeeNodes? Object.values(committeeNodes) : []} standByValidators={standByNodes ? Object.values(standByNodes) : []} />
    </Page>
 });
