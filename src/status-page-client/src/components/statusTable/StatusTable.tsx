42107070d95bbbf4534629ad8284287486378bc8
Or Lavy <orlavyil@gmail.com>
2020-09-14 17:29:27 +0200

    Moves status table header to its own component

--- src/status-page-client/src/components/statusTable/StatusTable.tsx
+++ src/status-page-client/src/components/statusTable/StatusTable.tsx
@@ -4,6 +4,7 @@ import { TABLE_ICONS } from '../tables/TableIcons';
 import { Button, Paper, Table, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core';
 import { VirtualChain, Guardian, Service } from '../../../../model/model';
 import { VcStatusCell } from './VcStatusCell';
+import { StatusTableHeader } from './StatusTableHeader';
 
 export interface ValidatorStatusGist {
   name: string;
@@ -31,65 +32,10 @@ export const StatusTable = React.memo<IProps>((props) => {
 
   console.log({ showServices });
 
-  const servicesHeaderCells = useMemo(() => {
-    if (!showServices) {
-      return null;
-    } else {
-      return services.map((service) => (
-        <TableCell>
-          <Typography>{service.Name}</Typography>
-        </TableCell>
-      ));
-    }
-  }, [services, showServices]);
-
-  const headerServicesPlaceholders = useMemo(() => {
-    if (!showServices) {
-      return null;
-    } else {
-      return services.map((service) => <TableCell />);
-    }
-  }, [services, showServices]);
-
-  // Build the top row (services and vcs)
-  const topRow = useMemo(() => {
-    const servicesCells = services.map((service) => <TableCell></TableCell>);
-
-    const onClick = showServices ? () => setShowServices(false) : () => setShowServices(true);
-
-    const topRow = (
-      <TableRow>
-        <TableCell></TableCell>
-        <TableCell>
-          <Button onClick={onClick}>{showServices ? 'Hide' : 'Show'}</Button>
-        </TableCell>
-        {servicesHeaderCells}
-        {vcs.map((vc) => (
-          <VcStatusCell key={vc.Id} vc={vc} />
-        ))}
-      </TableRow>
-    );
-
-    return topRow;
-  }, [services, servicesHeaderCells, vcs]);
-
-  // return <MaterialTable icons={TABLE_ICONS} columns={columns} data={validatorStatusGists}/>
   return (
     <TableContainer component={Paper}>
       <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Validators</TableCell>
-            <TableCell>Node services</TableCell>
-            {headerServicesPlaceholders}
-            {vcs.map((vc) => (
-              <TableCell key={vc.Id}>
-                {vc.Id} - {vc.Name}
-              </TableCell>
-            ))}
-          </TableRow>
-          {topRow}
-        </TableHead>
+        <StatusTableHeader services={services} vcs={vcs} setShowServices={setShowServices} showServices={showServices} />
       </Table>
     </TableContainer>
   );
---
+++ src/status-page-client/src/components/statusTable/StatusTableHeader.tsx
@@ -0,0 +1,73 @@
+import React, { useMemo } from 'react';
+import { Button, Table, TableCell, TableHead, TableRow, Typography } from '@material-ui/core';
+import { VcStatusCell } from './VcStatusCell';
+import { VirtualChain, Service } from '../../../../model/model';
+
+interface IProps {
+  vcs: VirtualChain[];
+  services: Service[];
+  showServices: boolean;
+  setShowServices: (val: boolean) => void;
+}
+
+export const StatusTableHeader = React.memo<IProps>((props) => {
+  const { showServices, vcs, services, setShowServices } = props;
+
+  const servicesHeaderCells = useMemo(() => {
+    if (!showServices) {
+      return null;
+    } else {
+      return services.map((service) => (
+        <TableCell>
+          <Typography>{service.Name}</Typography>
+        </TableCell>
+      ));
+    }
+  }, [services, showServices]);
+
+  const headerServicesPlaceholders = useMemo(() => {
+    if (!showServices) {
+      return null;
+    } else {
+      return services.map((service) => <TableCell />);
+    }
+  }, [services, showServices]);
+
+  // Build the top row (services and vcs)
+  const topRow = useMemo(() => {
+    const servicesCells = services.map((service) => <TableCell></TableCell>);
+
+    const onClick = showServices ? () => setShowServices(false) : () => setShowServices(true);
+
+    const topRow = (
+      <TableRow>
+        <TableCell></TableCell>
+        <TableCell>
+          <Button onClick={onClick}>{showServices ? 'Hide' : 'Show'}</Button>
+        </TableCell>
+        {servicesHeaderCells}
+        {vcs.map((vc) => (
+          <VcStatusCell key={vc.Id} vc={vc} />
+        ))}
+      </TableRow>
+    );
+
+    return topRow;
+  }, [services, servicesHeaderCells, setShowServices, showServices, vcs]);
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell>Validators</TableCell>
+        <TableCell>Node services</TableCell>
+        {headerServicesPlaceholders}
+        {vcs.map((vc) => (
+          <TableCell key={vc.Id}>
+            {vc.Id} - {vc.Name}
+          </TableCell>
+        ))}
+      </TableRow>
+      {topRow}
+    </TableHead>
+  );
+});
