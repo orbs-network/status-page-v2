3e016fb391e379d87a99e6d176c45251d4c02e8f
Or Lavy <orlavyil@gmail.com>
2020-09-21 17:47:47 +0200

    Finishes 'NodeVcStatusCell'

--- src/status-page-client/src/components/statusTable/ValidatorRow/NodeVcStatusCell.tsx
+++ src/status-page-client/src/components/statusTable/ValidatorRow/NodeVcStatusCell.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { NodeVirtualChain } from '../../../../../model/model';
 import { StatusCell } from './StatusCell';
 
@@ -9,12 +9,14 @@ interface IProps {
 export const NodeVcStatusCell = React.memo<IProps>((props) => {
   const { nodeVc } = props;
 
+  const subTitle = useMemo(() => `${nodeVc.Version} (${nodeVc.ProtocolVersion})`, [nodeVc.ProtocolVersion, nodeVc.Version]);
+
   return (
     <StatusCell
       healthLevel={nodeVc.Status}
-      title={nodeVc.StatusMsg}
-      titleLink={nodeVc.URLs.Status}
-      subTitle={nodeVc.Version}
+      title={nodeVc.BlockHeight.toLocaleString()}
+      // titleLink={nodeVc.URLs.Status}
+      subTitle={subTitle}
       subTitleLink={nodeVc.URLs.Version}
       tooltip={nodeVc.StatusToolTip}
       statusLink={nodeVc.URLs.Status}
