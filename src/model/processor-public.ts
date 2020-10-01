ed1dd01d801cea4bbd48a7ec5e1570405c8a070c
Noam Berg <noamberg@yahoo.com>
2020-10-01 19:44:39 +0300

    fix effective stake

--- src/model/processor-public.ts
+++ src/model/processor-public.ts
@@ -84,7 +84,7 @@ function readGuardians(rootNodeData: any): Guardians {
       Name: _.isString(guardianData.Name) ? guardianData.Name : '',
       Ip: ip,
       Website: _.isString(guardianData.Website) ? guardianData.Website : '',
-      EffectiveStake: _.isNumber(guardianData.EffectiveStake) ? guardianData.EffectiveStake : 1,
+      EffectiveStake: _.isNumber(guardianData.EffectiveStake) ? guardianData.EffectiveStake : 0,
       IsCertified: _.isNumber(guardianData.IdentityType) ? guardianData.IdentityType === 1 : false,
       OrbsAddress: _.isString(guardianData.OrbsAddress) ? guardianData.OrbsAddress : '',
       NodeManagementURL: URLs.generateNodeManagmentUrl(ip),
